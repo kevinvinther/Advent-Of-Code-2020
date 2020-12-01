9eb01cd5f214662d62979d580ce4570c4c477005
Kevin Vinther <Kevinjoshua.kjv@gmail.com>
2020-12-01 15:24:19 +0100

    Initial Commit

---
+++ Day_1/1.py
@@ -0,0 +1,24 @@
+advent_array = []
+
+with open("1_1.txt") as advent_input: 
+    for line in advent_input:
+        advent_array.append(int(line.removesuffix('\n')))
+
+def find_number(array):
+    for i in advent_array:
+        for y in advent_array: 
+            if i + y == 2020:
+                print("First number: " + str(i))
+                print("Second number: " + str(y))
+                print("Numbers added together: " + str(i+y))
+                print("Numbers multiplied:")
+                return i*y
+
+def find_number_3(array):
+    for i in advent_array: 
+        for y in advent_array: 
+            for z in advent_array:
+                if i + y + z == 2020:
+                    return i*y*z 
+
+print(find_number_3(advent_array))
